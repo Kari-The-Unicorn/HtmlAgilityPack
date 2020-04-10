da6fe2b0f5e0432f0238191ba737d1cc338a7d76
Karolina Roszkowska <inmatrixos@gmail.com>
2020-04-10 21:11:39 +0100

    added new simple htmlagility pack - scrape title of courses from website

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace ScrapeHtmlAgilityPack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load("http://www.c-sharpcorner.com");
+            HtmlNode[] nodes = document.DocumentNode.SelectNodes("//a[contains(@class, 'title')]").ToArray();
+            var csvFile = new StreamWriter("C:/Users/karol/OneDrive/Desktop/output/Sample.csv");
+            csvFile.Write("column" + ",");
+            foreach (HtmlNode item in nodes)
+            {
+                csvFile.Write(Environment.NewLine);
+                var data = item.InnerText;
+                csvFile.Write(data.Trim() + ",");
+            }
+        }
+    }
+}
