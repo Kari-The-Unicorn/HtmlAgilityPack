2bce558330e58978990066d6f6e9c67c00f7359e
Karolina Roszkowska <inmatrixos@gmail.com>
2021-03-25 21:29:55 +0000

    update

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using CsvHelper;
 using HtmlAgilityPack;
+using ScrapeHtmlAgilityPack.Helpers;
 
 namespace ScrapeHtmlAgilityPack
 {
@@ -13,15 +17,37 @@ namespace ScrapeHtmlAgilityPack
         {
             HtmlWeb web = new HtmlWeb();
             HtmlDocument document = web.Load(URL);
-            HtmlNode[] nodes = document.DocumentNode.SelectNodes("//a[contains(@class, 'title')]").ToArray();
-            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory) + "output/Sample.csv";
-            var csvFile = new StreamWriter(path);
-            csvFile.Write("column" + ",");
-            foreach (HtmlNode item in nodes)
+            HtmlNode eventsMain = document.DocumentNode.SelectNodes("//div[contains(@class,'Events')]").First();
+            HtmlNode[] events = eventsMain.SelectNodes(".//li").ToArray();
+            foreach (var evnt in events)
             {
-                csvFile.Write(Environment.NewLine);
-                var data = item.InnerText;
-                csvFile.Write(data.Trim() + ",");
+                HtmlNode[] dateTimeDetails = evnt.SelectNodes("//span[contains(@class, 'timeEvent')]").ToArray();
+                foreach (var info in dateTimeDetails)
+                {
+                    string month = info.SelectSingleNode("//span[contains(@class, 'LocalMonth1')]").InnerText;
+                    string day = info.SelectSingleNode("//span[contains(@class, 'LocalDay1')]").InnerText;
+                    string time = info.SelectSingleNode("//span[contains(@class, 'LocalTime1')]").InnerText;
+                    string dateTime = day + " " + month + " " + time;
+                    
+
+
+                }
+            }
+        }
+        private void WriteToCsv(string dateTime, string subject, string registerDetails)
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory) + "output/Sample.csv";
+            using (var writer = new StreamWriter(path))
+            using (var csvWriter = new CsvWriter(writer, CultureInfo.GetCultureInfo("en-GB")))
+            {
+                var records = new List<Header>();
+                records.Add(new Header
+                {
+                    DateTime = dateTime,
+                    Subject = subject,
+                    RegisterDetails = registerDetails,
+                });
+                csvWriter.WriteRecords(records);
             }
         }
     }
